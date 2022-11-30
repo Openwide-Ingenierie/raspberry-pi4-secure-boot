5876438336e21e76d8c0847faa33920a35c027fa
Tim Gover <tim.gover@raspberrypi.com>
2022-11-30 09:47:16 +0000

    package/rpi-eeprom: new package for RPi bootloader tools
    This package adds the host and device tools for modifying,
    signing Raspberry Pi bootloader ramdisk (boot.img) files.
    
    Normally, the host tools would be invoked from a post-build
    script to sign the boot.img file. The device tools would be
    invoked from a custom on-device software update process e.g.
    using rpi-eeprom-digest to verify the signature of a boot.img
    file against a public key retrieved from the EEPROM.
    
    The current version does not download the EEPROM images
    because the bootloader EEPROM is not normally updated via
    a buildroot image. However, future versions might provide
    the facility to generate both a buildroot initramfs and
    accompanying signed EEPROM image e.g for network install.

---
+++ package/rpi-eeprom/rpi-eeprom.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# rpi-eeprom
+#
+################################################################################
+
+RPI_EEPROM_VERSION = 0ebda77d49ba90927fa1214bb7068cd744e7d811
+RPI_EEPROM_SITE = $(call github,raspberrypi,rpi-eeprom,$(RPI_EEPROM_VERSION))
+RPI_EEPROM_LICENSE = BSD-3-Clause
+RPI_EEPROM_LICENSE_FILES = LICENSE
+
+HOST_RPI_EEPROM_INSTALL = YES
+RPI_EEPROM_INSTALL = YES
+
+define HOST_RPI_EEPROM_INSTALL_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/rpi-eeprom-digest $(HOST_DIR)/bin/rpi-eeprom-digest
+	$(INSTALL) -D -m 0755 $(@D)/rpi-eeprom-config $(HOST_DIR)/bin/rpi-eeprom-config
+endef
+
+define RPI_EEPROM_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/rpi-eeprom-digest $(TARGET_DIR)/bin/rpi-eeprom-digest
+	$(INSTALL) -D -m 0755 $(@D)/tools/rpi-bootloader-key-convert $(TARGET_DIR)/bin/rpi-bootloader-key-convert
+	$(INSTALL) -D -m 0755 $(@D)/tools/rpi-otp-private-key $(TARGET_DIR)/bin/rpi-otp-private-key
+endef
+
+$(eval $(generic-package))
+$(eval $(host-generic-package))
