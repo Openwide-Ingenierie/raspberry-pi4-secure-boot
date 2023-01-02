95e3fa2595252863ea7eea42569c8f95dea10d81
Ever ATILANO <ever.atilano@smile.fr>
2023-01-02 15:33:14 +0100

    raspberrypi-usbboot: bump version to 20221215-105525
    Adapted mk for the upgraded version

--- package/raspberrypi-usbboot/raspberrypi-usbboot.mk
+++ package/raspberrypi-usbboot/raspberrypi-usbboot.mk
@@ -4,9 +4,9 @@
 #
 ################################################################################
 
-RASPBERRYPI_USBBOOT_VERSION = 2021.07.01
+RASPBERRYPI_USBBOOT_VERSION = 20221215-105525
 RASPBERRYPI_USBBOOT_SITE = \
-	$(call github,raspberrypi,usbboot,v$(RASPBERRYPI_USBBOOT_VERSION))
+	$(call github,raspberrypi,usbboot,$(RASPBERRYPI_USBBOOT_VERSION))
 RASPBERRYPI_USBBOOT_LICENSE = Apache-2.0
 RASPBERRYPI_USBBOOT_LICENSE_FILES = LICENSE
 
@@ -17,7 +17,9 @@ define HOST_RASPBERRYPI_USBBOOT_BUILD_CMDS
 endef
 
 define HOST_RASPBERRYPI_USBBOOT_INSTALL_CMDS
-	$(INSTALL) -D -m 0755 $(@D)/rpiboot $(HOST_DIR)/bin/rpiboot
+	$(INSTALL) -D -m 0644 $(@D)/secure-boot-recovery/bootcode4.bin $(BINARIES_DIR)/recovery.bin
+	$(INSTALL) -D -m 0644 $(@D)/secure-boot-recovery/pieeprom.original.bin $(BINARIES_DIR)
+	$(INSTALL) -D -m 0644 $(@D)/secure-boot-recovery/boot.conf $(BINARIES_DIR)
 endef
 
 $(eval $(host-generic-package))
